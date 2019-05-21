450e8acd9199c1de4c4dbe300790e9cd2221c962
Laura Badgley <l.badgleyy@gmail.com>
2019-05-21 10:39:30 -0700

    app rendering/ test green

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import Blue from './BlueComponent';
+import Yellow from './YellowComponent';
+import Red from './RedComponent';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Header} />
+        <Route path="/blue" component={Blue} />
+        <Route path="/yellow" component={Yellow} />
+        <Route path="/red" component={Red} />
+      </Switch>
+    </Router>
+  );
+}
