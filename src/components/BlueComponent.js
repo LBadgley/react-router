a0cc406e2ecee8c5cae4b88fd79fb90fd2db5de2
Laura Badgley <l.badgleyy@gmail.com>
2019-05-21 10:40:02 -0700

    BlueComponent test green

---
+++ src/components/BlueComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BlueComponent() {
+  const splotchStyle = {
+    width: '550px',
+    height: '550px',
+    backgroundColor:'blue'
+  };
+  return <div style={splotchStyle}></div>;
+}
